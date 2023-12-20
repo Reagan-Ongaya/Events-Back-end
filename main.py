8edec3ca32a722bf31d2189405d5e2906d5a08ee
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-20 07:56:08 +0300

    setting routes

--- main.py
+++ main.py
@@ -2,8 +2,50 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Creating routes for our server
+
 @app.get('/')
 def index():
     return{
         "massage" :"Welcome"
+    }
+
+#getting all destination
+@app.get('/destinations')
+def destinations():
+    return[]
+
+
+#getting a single destination
+@app.get('/destinations/{destination_id}')
+def event():
+    return []
+
+
+#post a destination
+@app.post('/')
+def create():
+    return{
+        "message":"Created with success"
+    }
+    
+@app.post('/destinations')
+def create_destinations():
+    return{
+        "message":"Destination created with success"
+    }
+    
+    
+ #updating   
+@app.patch('/destinations/{destination_id}')
+def update_destination(destination_id : int):
+    return{
+        "message":f"Destination { destination_id }created with success"
+    }
+    
+# deletion
+@app.delete('/destinations/{destination_id}')
+def delete_destination(destination_id : int):
+    return{
+        "message":f"Destination { destination_id }deleted with success"
     }
